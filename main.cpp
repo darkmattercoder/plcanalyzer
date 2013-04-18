ef0c48edc1c8b697fe43fd88bc2f62ca3f6df1d0
NevelSavage <kernlukas@web.de>
2013-04-18 22:07:23 +0200

    HellOP rintfzeug

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #define LINUX
 #include <QtGui/QApplication>
+#include <iostream>
 #include "mainwindow.h"
-
+using namespace std;
+void printUnnuetzesZeug();
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
+printUnnuetzesZeug();
     return a.exec();
 }
+
+void printUnnuetzesZeug()
+{cout << "Hello LuKe" << endl;}
