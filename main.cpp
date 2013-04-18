642ceab8db6f22d61bcc325b3e7274b62971527a
NevelSavage <kernlukas@web.de>
2013-04-18 23:00:17 +0200

    Menü -> Neues Projekt: Es wird "Hello World" in der Konsole ausgegeben

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #define LINUX
 #include <QtGui/QApplication>
-#include <iostream>
 #include "mainwindow.h"
-using namespace std;
-void printUnnuetzesZeug();
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-printUnnuetzesZeug();
+
     return a.exec();
 }
 
-void printUnnuetzesZeug()
-{cout << "Hello LuKe" << endl;}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -14,7 +14,13 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+public slots:
+    void printUnnuetzesZeug();
     
+private slots:
+    void on_actionNeues_Projekt_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
